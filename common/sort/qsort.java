f2e3244b5c87731067f68aee0e668f23627cfadd
Marveliu <897920245@qq.com>
2018-11-20 11:38:35 +0800

    add:qsort

---
+++ common/sort/qsort.java
@@ -0,0 +1,51 @@
+//  快速排序实现
+
+class qsort {
+
+    /**
+     * 
+     * @param s 被排序的数组
+     * @param p 排序上标 0
+     * @param r 排序下标 length
+     * @return
+     */
+    public static void quickSort(int[] s, int p, int r) {
+        if (p < r) {
+            int q = partition(s, p, r);
+            // q 为分割点，不会再参与排序
+            quickSort(s, p, q);
+            quickSort(s, q + 1, r);
+            }
+    }
+
+    /**
+     * 划分
+     * 
+     * @param s
+     * @param p
+     * @param r
+     * @return 返回排序分割点
+     */
+    private static int partition(int[] s, int p, int r) {
+        int key = s[r - 1];
+        int i = p - 1;
+        int j = p;
+        for (; j < r - 1; j++) {
+            if (s[j] <= key) {
+                i++;
+                int temp = s[j];
+                s[j] = s[i];
+                s[i] = temp;
+            }
+        }
+        s[r - 1] = s[i + 1];
+        s[i + 1] = key;
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] a = { 100, 2, 3 };
+        qsort.quickSort(a, 0, a.length);
+        LogUtil.log(a);
+    }
+}
