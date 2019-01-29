9e030c261cd95f486b9983c2be117687f9e8b41c
Marveliu <897920245@qq.com>
2019-01-29 23:31:10 +0800

    update

---
+++ leetcode/56.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+// 96. Unique Binary Search Trees
+// 思路：dp
+// 参考：https://leetcode.com/problems/unique-binary-search-trees/discuss/31666/DP-Solution-in-6-lines-with-explanation.-F(i-n)-G(i-1)-*-G(n-i)
+public class Solution {
+
+    // G(n): the number of unique BST for a sequence of length n.
+    // F(i, n), 1 <= i <= n: the number of unique BST, where the number i is the
+    // root of BST, and the sequence ranges from 1 to n.
+    // G(n) = F(1, n) + F(2, n) + ... + F(n, n).
+    // The tricky part is that we could consider the number of unique BST out of sequence [1,2] as G(2).... => F(i, n) = G(i-1) * G(n-i) 1 <= i <= n
+    //
+    public int numTrees(int n) {
+        int[] G = new int[n + 1];
+        G[0] = G[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+
+        return G[n];
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Input>>>");
+            int ans = s.numTrees(sc.nextInt());
+            System.out.println(ans);
+        }
+    }
+}
---
+++ leetcode/57.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+// 93. Restore IP Addresses
+// 思路：dfs
+public class Solution {
+
+    List<String> ans = null;
+
+    public List<String> restoreIpAddresses(String s) {
+        ans = new ArrayList<>();
+        if (s == null || s.length() < 4 || s.length() > 12)
+            return ans;
+
+        getIp("", s, 0);
+        return ans;
+    }
+
+    public void getIp(String ip, String s, int count) {
+        if (count == 4 && s.isEmpty()) {
+            ans.add(ip);
+            return;
+        }
+        if (!ip.isEmpty())
+            ip = ip + ".";
+        if (!s.isEmpty()) {
+            for (int i = 1; i <= Math.min(3, s.length()); i++) {
+                String now = s.substring(0, i);
+                if (isIp(now)) {
+                    getIp(ip + now, s.substring(i, s.length()), count + 1);
+                }
+            }
+        }
+    }
+
+    public static boolean isIp(String s) {
+        if (s.length() <= 3) {
+            if (s.length() > 1 && s.startsWith("0")) {
+                return false;
+            }
+            int n = Integer.valueOf(s);
+            if (n >= 0 && n <= 255) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Input>>>");
+            String in = sc.nextLine();
+            List<String> ans = s.restoreIpAddresses(in);
+            System.out.println(Arrays.toString(ans.toArray()));
+        }
+    }
+}
+
+// 010010
