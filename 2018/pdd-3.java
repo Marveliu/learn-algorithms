c9686820c632fa2227aa881ea635525e645db368
Marveliu <897920245@qq.com>
2019-03-10 23:17:22 +0800

    update

---
+++ 2018/pdd-3.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    // public int threeSumClosest(int[] nums, int target) {
+    //     Arrays.sort(nums);
+    //     int start = 0;
+    //     int end = nums.length;
+    //     int close 
+        
+    // }
+    
+    // public int search(int[] nums,int target){
+    //     int start  = 0;
+    //     int end = nums.length;
+    //     while(start < end){
+    //         if(start + 1 == end){
+    //             return Math.abs(nums[start] - target) > Math.abs(nums[end] - target) ? end : start;
+    //         }
+    //         int mid = (start + end)/2;
+    //         if(nums[mid] == target){
+    //             return mid;
+    //         }else if(nums[mid] < target){
+    //             start = mid;
+    //         }else{
+    //             end = mid;
+    //         }
+    //     }
+    // }
+}
